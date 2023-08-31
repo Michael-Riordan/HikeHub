a927357af1c0704d27bf2e9108aa010490d7c335
Michael <michael.riordan81@outlook.com>
2023-08-31 09:30:44 -0700

    add request to recreation.gov api

--- src/components/AdventureAutocomplete.jsx
+++ src/components/AdventureAutocomplete.jsx
@@ -1,13 +1,29 @@
 import { Autocomplete } from "@mui/material";
 import { TextField } from "@mui/material";
+import { useEffect, useState, useCallback } from "react";
 
 export default function AdventureAutocomplete() {
-    const adventures = [
-        'Hiking',
-        'Biking',
-        'Camping',
-        'Fishing',
-    ]
+    const [activities, setActivities] = useState([]);
 
-    return <Autocomplete renderInput={(params) => <TextField {...params} label='Adventures' color="success" />} options={adventures} id='Autocomplete'/>
+    const handleAdventureSelect = () => {
+        console.log('selected');
+    }
+
+    useEffect(() => {
+        const activityList = [];
+
+        const fetchActivities = async () => {
+            console.log('fetching');
+            const response = await fetch('https://ridb.recreation.gov/api/v1/activities?limit=50&offset=0&apikey=9bf6a5ef-ff3a-406f-8c75-8663720bc514');
+            const jsonResponse = await response.json();
+            jsonResponse.RECDATA.forEach((activity) => {
+                activityList.push(activity.ActivityName);
+            });
+            setActivities(activityList);
+        }
+
+        fetchActivities();
+    }, []);
+
+    return <Autocomplete sx={{width: 400}} renderInput={(params) => <TextField {...params} label='Adventures' color="success" hiddenLabel={true} variant="filled" id='autocomplete-textfield'/>} options={activities} id='Autocomplete' />
 }
